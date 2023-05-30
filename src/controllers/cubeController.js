b24466878be14d7c6c3761af9956046b1292f8e8
Georgi Todorov <106379153+GeorgiTTodorov@users.noreply.github.com>
2023-05-31 02:03:39 +0300

    Added cube details and 404 page

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -4,7 +4,7 @@ const cubeManager = require('../managers/cubeManagers.js');
 
 // Path /cubes/create
 router.get("/create", (req, res) => {
-    console.log(cubeManager.getAll());
+    
     res.render("create");
 });
 
@@ -25,6 +25,16 @@ router.post("/create", (req, res) => {
     });
 
     res.redirect("/");
+});
+
+router.get('/:cubeId/details', (req,res) => {
+    const cube = cubeManager.getOne(req.params.cubeId);
+
+    if(!cube) {
+        res.redirect('/404');
+    }
+
+    res.render('details', {cube})
 });
 
 module.exports = router;
--- src/managers/cubeManagers.js
+++ src/managers/cubeManagers.js
@@ -1,11 +1,23 @@
-const cubes = [];
+const uniqid = require('uniqid');
+
+const cubes = [
+    {
+        id: '76yiwx5t4liauldhr',
+        name: 'Hellraiser\'s cube',
+        description: 'A hellishly fun cube',
+        imageUrl: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.nkcqLiMLQaZ2k5s7Hl2gYwHaHu%26pid%3DApi&f=1&ipt=474d3256769fdd512c78a3d463fdc38862ee54b33c3a711ace3bf4d76c939a3c&ipo=images',
+        difficultyLevel: 6,
+    }
+];
 
 exports.getAll = () => cubes.slice();
 
-exports.create = (cubeData) => {
+exports.getOne = (cubeId) => cubes.find(x => x.id === cubeId);
+
+exports.create = (cube) => {
     const newCube = {
-        id: cubes.length + 1,
-        ...cubeData
+        id: uniqid(),
+        ...cube
     }
 
     cubes.push(newCube);
