eef78e0cb2138a36356a9574543fbcc4041ddd27
Georgi Todorov <106379153+GeorgiTTodorov@users.noreply.github.com>
2023-06-08 01:15:13 +0300

    Added cube model, create cube and cube details

--- src/managers/cubeManagers.js
+++ src/managers/cubeManagers.js
@@ -1,24 +1,8 @@
-const uniqid = require('uniqid');
+const Cube = require('../models/Cube.js');
 
-const cubes = [
-    {
-        id: '76yiwx5t4liauldhr',
-        name: 'Hellraiser\'s cube',
-        description: 'A hellishly fun cube',
-        imageUrl: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.nkcqLiMLQaZ2k5s7Hl2gYwHaHu%26pid%3DApi&f=1&ipt=474d3256769fdd512c78a3d463fdc38862ee54b33c3a711ace3bf4d76c939a3c&ipo=images',
-        difficultyLevel: 6,
-    },
-    {
-        id: '2n73sh8holaz66elc',
-        name: 'Rubic Classic',
-        description: 'Evergreen',
-        imageUrl: 'https://www.hpcwire.com/wp-content/uploads/2018/07/Rubiks_Cube_shutterstock_271810067-675x380.jpg',
-        difficultyLevel: 3
-    }
-];
 
 exports.getAll = (search, from, to) => {
-    let result = cubes.slice();
+    let result = Cube.find().lean();
 
     if (search) {
         result = result.filter(cube => cube.name.toLowerCase().includes(search.toLowerCase()));
@@ -35,15 +19,12 @@ exports.getAll = (search, from, to) => {
     return result;
 };
 
-exports.getOne = (cubeId) => cubes.find(x => x.id === cubeId);
+exports.getOne = (cubeId) => Cube.findById(cubeId);
 
-exports.create = (cubeData) => {
-    const newCube = {
-        id: uniqid(),
-        ...cubeData
-    }
+exports.create = async (cubeData) => {
+    const cube = new Cube(cubeData);
 
-    cubes.push(newCube);
+    await cube.save();
 
-    return newCube;
+    return cube;
 }
