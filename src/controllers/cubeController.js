49a96fa7447a94713576411a20aef8c8debdc926
Georgi Todorov <106379153+GeorgiTTodorov@users.noreply.github.com>
2023-06-08 17:24:13 +0300

    render the accessories

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -31,6 +31,7 @@ router.post("/create", async (req, res) => {
 router.get('/:cubeId/details', async (req,res) => {
     
     const cube = await cubeManager.getOne(req.params.cubeId).lean();
+    console.log(cube);
     
     if(!cube) {
         res.redirect('/404');
